b6fd2dc383bd9d4891feb05a55cae74bffc23400
UTexas80 <glen.falk@utexas.edu>
2020-06-05 16:15:09 -0400

    20200605osfaFundsB
    a. create matrix to add 3 columns into total(s)

--- munge/02-processing.R
+++ munge/02-processing.R
@@ -1,27 +1,38 @@
-
-# --------------------------------------------------------------------------
-dt_osfa_term_fall <- dt_osfa_term[order(AY, Fund_Code), c(1:21)]
-# --------------------------------------------------------------------------
-# dynamic column index                        # https://tinyurl.com/y7pjxqd6
-#  -------------------------------------------------------------------------
-iEven <- seq(6, 20, 2)
-iOdd   <- seq(7, 53, 2)
+# -------------------------------------------------------------------------
+# dynamic column index                       # https://tinyurl.com/y7pjxqd6
+# -------------------------------------------------------------------------
+iEven             <- seq(6, 20, 2)
+iOdd              <- seq(7, 53, 2)
+# -------------------------------------------------------------------------
 paste(as.character(iOdd), collapse = ", ")
-dt_osfa_term[, ..iOdd]
+# -------------------------------------------------------------------------
+# lapply multiple functions                 # https://tinyurl.com/y9hxhq2p
+# -------------------------------------------------------------------------
+dt_osfa_term[, c(7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29,
+  31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53)] <- dt_osfa_term[,
+  lapply(dt_osfa_term[, ..iOdd], function(x) {
+    as.integer(trimws(gsub("[\\$,]", "", x), "b"))
+  })]
+# --------------------------------------------------------------------------Sum column every n column in a data frame R
+a <- seq(6,21,by=1)
+b <- seq(22,37,by=1)
+c <- seq(38,53,by=1)
+rbind(a,b,c)
+add_column <- cbind(a,b,c)
+
+df <- data.frame(replicate(expr=rnorm(100),n = 10))
+sapply(seq(1,9,by=2),function(i) rowSums(df[,i:(i+1)]))
+# -------------------------------------------------------------------------
+# dt_osfa_term[, ..iOdd] <- dt_osfa_term[, lapply(dt_osfa_term[,
+#   ..iOdd], function(x) {
+#   as.integer(trimws(gsub("[\\$,]", "", x), "b"))
+# })]
 # --------------------------------------------------------------------------
-# lapply multiple functions                   # https://tinyurl.com/y9hxhq2p
-#  -------------------------------------------------------------------------
-dt_osfa_term_fall[, c(7, 9, 11, 13, 15, 17, 19, 21)] <- dt_osfa_term_fall[,
-  lapply(dt_osfa_term_fall[, c(7, 9, 11, 13, 15, 17, 19, 21)],
-    function(x) {
-      as.integer(trimws(gsub("[\\$,]", "", x), "b"))
-    })]
-# --------------------------------------------------------------------------
-i1 <- seq(6, 20, 1)
-dt_osfa_term_fall_ay <- dt_osfa_term_fall[, lapply(.SD, sum,
-  na.rm = TRUE), by = AY, .SDcols = i1]
-dt_osfa_term_fall_ay_fund <- dt_osfa_term_fall[, lapply(.SD, sum,
-  na.rm = TRUE), by = c("AY","Fund_Code", "Fund_Title"), .SDcols = i1]
+# i1 <- seq(6, 20, 1) dt_osfa_term_fall_ay <-
+# dt_osfa_term_fall[, lapply(.SD, sum, na.rm = TRUE), by =
+# AY, .SDcols = i1] dt_osfa_term_fall_ay_fund <-
+# dt_osfa_term_fall[, lapply(.SD, sum, na.rm = TRUE), by =
+# c('AY','Fund_Code', 'Fund_Title'), .SDcols = i1]
 # --------------------------------------------------------------------------
 # VERSION HISTORY
 # --------------------------------------------------------------------------
